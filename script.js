ffa486b87b366c6a6ebc8aa723dfa2aa09a16a0c
takutoakeya <109582984+takutoakeya@users.noreply.github.com>
2023-10-13 16:05:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // JavaScript
-//console.log(document.getElementById('myColor').value);
 
-//定数の宣言
 const color = document.getElementById('myColor');
 const text = document.getElementById('bodyText');
 
-//document.getElementById('bodyText').textContent =document.getElementById('myColor').value;
+//背景色を変更する関数
+//function構文を使う書き方
+function changeColor(){
+  document.body.style.backgroundColor = color.value;
 
-text.textContent = color.value;
+  //カラーコードを表示
+  if (color.value === '#ffffff') {
+    text.textContent = 'コード：' + color.value + '(white)';
+  } else if (color.value === '#000000') {
+    text.textContent = 'コード：' + color.value + '(black)';
+  } else {
+    text.textContent = 'コード：' + color.value;
+  }
 
+}
+
+//アロー関数を使う書き方
+// const changeColor = () => {
+//   document.body.style.backgroundColor = color.value;
+//   text.textContent = color.value;
+// }
+
+// カラーピッカーが変更されたときに、背景色を変える
+color.addEventListener('input', changeColor);
